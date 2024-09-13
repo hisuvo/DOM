11d982cbd86c2bda01c74e36d1274c3287c0f7e0
suvo <suvodatta72@gmail.com>
2024-09-14 01:22:22 +0600

    done

--- project/clrGenerate/project-1/script.js
+++ project/clrGenerate/project-1/script.js
@@ -1,1 +1,13 @@
 
+let bg = document.getElementById("body");
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", function() {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    
+    let rgb = `rgb(${red} ${green} ${blue})`
+    btn.textContent= rgb; 
+    bg.style.backgroundColor = rgb
+})
